822ce2c8a4025e94387b26d4899f8a5403ebcffc
Victor Tran <victor-likes-coding@tempestas.io>
2023-02-14 18:01:14 -0500

    write todo logic

--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
 
 export default class Todo extends React.Component {
-  render() {
-    return (
-      <div>
-        Todo
-      </div>
-    )
-  }
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const { data } = this.props;
+        /* todo structure
+        {
+          name: 'Organize Garage',
+          id: 1528817077286, // could look different, you could use a timestamp to generate it
+          completed: false
+        }
+    */
+
+        // check if todo is completed and add a checkmark if it is
+        return (
+            <li onClick={(e) => this.props.onClickHandler(e, data.id)}>
+                {data.name} {data.completed && "\u2713"}
+            </li>
+        );
+    }
 }
