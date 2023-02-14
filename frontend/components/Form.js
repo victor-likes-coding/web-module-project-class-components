32492fb5b7d83cbfb082bbb1c04db296de484c5a
Victor Tran <victor-likes-coding@tempestas.io>
2023-02-14 18:01:44 -0500

    write form logic for adding todos

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
 
 export default class Form extends React.Component {
-  render() {
-    return (
-      <div>
-        Form
-      </div>
-    )
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "",
+        };
+    }
+
+    handleChange = (e) => {
+        this.setState({ ...this.state, [e.target.name]: e.target.value });
+    };
+
+    render() {
+        return (
+            <form
+                onSubmit={(e) => {
+                    this.props.addTodo(e, this.state);
+                    this.setState((state) => ({ ...state, name: "" }));
+                }}>
+                <input
+                    type="text"
+                    name="name"
+                    value={this.state.name}
+                    onChange={this.handleChange}
+                />
+                <input
+                    type="submit"
+                    value="Submit"
+                />
+            </form>
+        );
+    }
 }
