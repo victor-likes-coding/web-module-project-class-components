1951de2357341bf2af8756f982061b35fbe067f8
Victor Tran <victor-likes-coding@tempestas.io>
2023-02-14 18:02:14 -0500

    import components

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from "react";
+import TodoList from "./TodoList";
+import Form from "./Form";
 
 export default class App extends React.Component {
     constructor(props) {
