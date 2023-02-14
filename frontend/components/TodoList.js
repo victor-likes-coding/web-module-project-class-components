868039657c24a14b75756327eff4ebb81f0d200f
Victor Tran <victor-likes-coding@tempestas.io>
2023-02-14 18:01:31 -0500

    write todo list logic

--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import Todo from "./Todo";
 
 export default class TodoList extends React.Component {
-  render() {
-    return (
-      <div>
-        TodoList
-      </div>
-    )
-  }
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                {this.props.showCompleted
+                    ? this.props.todos.map((todo) => (
+                          <Todo
+                              data={todo}
+                              key={todo.id}
+                              onClickHandler={this.props.onClickHandler}
+                          />
+                      ))
+                    : this.props.todos.map(
+                          (todo) =>
+                              !todo.completed && (
+                                  <Todo
+                                      data={todo}
+                                      key={todo.id}
+                                      onClickHandler={this.props.onClickHandler}
+                                  />
+                              )
+                      )}
+            </div>
+        );
+    }
 }
